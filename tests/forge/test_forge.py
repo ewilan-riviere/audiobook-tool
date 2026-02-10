e0867133ce532de9e4d2333b14f7f3655fcbb8b3
Ewilan Rivière <ewilan.riviere@gmail.com>
2026-02-10 18:42:20 +0100

    fix tests

--- tests/forge/test_forge.py
+++ tests/forge/test_forge.py
@@ -1,18 +1,29 @@
-from src.audiobook.forge import AudiobookForge
-import src.audiobook.utils as utils
-from src.audiobook.audio import AudioReader
+import tempfile
+from pathlib import Path
+from audiobook.forge import AudiobookForge
+from audiobook.audio import AudioReader
 
 
 def test_forge():
-    mp3_dir = "./tests/media/the-wall"
-    forge = AudiobookForge(mp3_dir, True)
-    forge = forge.build()
+    mp3_directory = "./tests/media/the-wall"
+    temporary_directory = tempfile.TemporaryDirectory()
+    forge = AudiobookForge(
+        source_path=Path(mp3_directory),
+        working_directory=Path(temporary_directory.name),
+        clear=True,
+    ).run()
 
-    assert forge.blacksmith
-    assert len(forge.blacksmith.mp3_files) == 2
-    assert len(forge.blacksmith.chapters) == 2
-    assert forge.blacksmith.target_bitrate == "128k"
-    assert utils.file_exists(forge.m4b_file) is True
+    m4b_file = Path(forge.output_path)
 
-    reader = AudioReader(forge.m4b_file)
-    print(reader)
+    reader = AudioReader(m4b_file)
+    assert m4b_file.exists() is True
+    assert reader.container.extension == "m4b"
+    assert len(reader.tags.chapters) == 5
+    assert reader.properties.bit_rate == 193661
+    assert reader.properties.duration_ms == 100060
+
+    assert reader.container.writable is True
+    assert reader.container.readable is True
+    assert reader.container.is_file is True
+
+    temporary_directory.cleanup()
