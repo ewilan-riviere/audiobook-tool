14d40fd690c7ce21060b028366a0514a3928d946
Ewilan Rivière <ewilan.riviere@gmail.com>
2026-02-16 12:22:40 +0100

    fix

--- tests/yml/test_reader.py
+++ tests/yml/test_reader.py
@@ -18,9 +18,8 @@ def test_reader():
     assert metadata.narrators == "Rob Inglis & Andy Serkis"
     assert isinstance(metadata.description, str)
     assert isinstance(metadata.lyrics, str)
-    assert (
-        metadata.copyright
-        == "©1954 The Tolkien Estate Limited (P)2025 HarperCollins Publishers Limited"
+    assert metadata.copyright == (
+        "©1954 The Tolkien Estate Limited (P)2025" "HarperCollins Publishers Limited"
     )
     assert metadata.genres == "Fantasy/Fiction"
     assert metadata.series == "The Lord of the Rings"
@@ -42,9 +41,8 @@ def test_reader():
     assert audiobook.composer == "Rob Inglis & Andy Serkis"
     assert audiobook.genre == "Fantasy/Fiction"
     assert audiobook.date == "2005"
-    assert (
-        audiobook.copyright
-        == "©1954 The Tolkien Estate Limited (P)2025 HarperCollins Publishers Limited"
+    assert audiobook.copyright_ == (
+        "©1954 The Tolkien Estate Limited (P)2025" "HarperCollins Publishers Limited"
     )
     assert audiobook.comment is None
     assert isinstance(audiobook.description, str)
