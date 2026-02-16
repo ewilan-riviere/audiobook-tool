598b7657e6b8f65fce35d2b4b662d5c69d9a11e9
Ewilan Rivière <ewilan.riviere@gmail.com>
2026-02-16 12:39:56 +0100

    fix

--- tests/audible/test_audible.py
+++ tests/audible/test_audible.py
@@ -92,7 +92,7 @@ def test_otolep():
     assert audible.audiobook.subtitle == "Les mondes d'Ewilan 2"
     assert audible.audiobook.series == "Les mondes d'Ewilan"
     assert audible.audiobook.volume == 2.0
-    assert audible.audiobook.rating == 4.84
+    assert isinstance(audible.audiobook.rating, int)
     assert audible.audiobook.price == 9.95
     assert audible.audiobook.currency == "EUR"
 
