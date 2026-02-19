7ca1a5770b448438a5e59b23fd9b50c1ba9bee46
Ewilan Rivière <ewilan.riviere@gmail.com>
2026-02-19 10:17:04 +0100

    fixes

--- tests/audible/test_locale.py
+++ tests/audible/test_locale.py
@@ -1,5 +1,5 @@
-# from audiobook.audible import Audible
-# from audiobook.models import AudibleAudiobook
+from audiobook.audible import Audible
+from audiobook.models import AudibleAudiobook
 
 ASIN_FR = "B00945ME60"
 ASIN_CO_UK = "B004FTL55Q"
@@ -7,45 +7,45 @@ ASIN_COM = "B003EYRWCS"
 ASIN_DE = "B004V03H3W"
 
 
-# def test_audible_fr():
-#     audible = Audible(ASIN_FR, "fr")
-#     _test_halway_to_the_grave(audible.audiobook)
+def test_audible_fr():
+    audible = Audible(ASIN_FR, "fr")
+    _test_halway_to_the_grave(audible.audiobook)
 
 
-# def test_audible_fr_auto():
-#     audible = Audible(ASIN_FR)
-#     _test_halway_to_the_grave(audible.audiobook)
+def test_audible_fr_auto():
+    audible = Audible(ASIN_FR)
+    _test_halway_to_the_grave(audible.audiobook)
 
 
-# def test_audible_co_uk():
-#     audible = Audible(ASIN_CO_UK, "co.uk")
-#     _test_halway_to_the_grave(audible.audiobook)
+def test_audible_co_uk():
+    audible = Audible(ASIN_CO_UK, "co.uk")
+    _test_halway_to_the_grave(audible.audiobook)
 
 
-# def test_audible_co_uk_uk():
-#     audible = Audible(ASIN_CO_UK)
-#     _test_halway_to_the_grave(audible.audiobook)
+def test_audible_co_uk_uk():
+    audible = Audible(ASIN_CO_UK)
+    _test_halway_to_the_grave(audible.audiobook)
 
 
-# def test_audible_com():
-#     audible = Audible(ASIN_COM, "com")
-#     _test_halway_to_the_grave(audible.audiobook)
+def test_audible_com():
+    audible = Audible(ASIN_COM, "com")
+    _test_halway_to_the_grave(audible.audiobook)
 
 
-# def test_audible_com_auto():
-#     audible = Audible(ASIN_COM)
-#     _test_halway_to_the_grave(audible.audiobook)
+def test_audible_com_auto():
+    audible = Audible(ASIN_COM)
+    _test_halway_to_the_grave(audible.audiobook)
 
 
-# def test_audible_de():
-#     audible = Audible(ASIN_DE, "de")
-#     _test_halway_to_the_grave(audible.audiobook)
+def test_audible_de():
+    audible = Audible(ASIN_DE, "de")
+    _test_halway_to_the_grave(audible.audiobook)
 
 
-# def test_audible_de_auto():
-#     audible = Audible(ASIN_DE)
-#     _test_halway_to_the_grave(audible.audiobook)
+def test_audible_de_auto():
+    audible = Audible(ASIN_DE)
+    _test_halway_to_the_grave(audible.audiobook)
 
 
-# def _test_halway_to_the_grave(audiobook: AudibleAudiobook):
-#     assert audiobook.title == "Halfway to the Grave"
+def _test_halway_to_the_grave(audiobook: AudibleAudiobook):
+    assert audiobook.title == "Halfway to the Grave"
